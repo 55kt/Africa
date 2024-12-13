109bfe11fb1f09b9b9f157a49f1b57779cc67eef
Vlad <160039489+55kt@users.noreply.github.com>
2024-12-13 19:00:53 +0200

    Dinamic animal list

---
+++ Africa/View/AnimalListItemView.swift
@@ -0,0 +1,44 @@
+//
+//  AnimalListItemView.swift
+//  Africa
+//
+//  Created by Vlad on 13/12/24.
+//
+
+import SwiftUI
+
+struct AnimalListItemView: View {
+    // MARK: - Properties
+    let animal: Animal
+    
+    // MARK: - Body
+    var body: some View {
+        HStack(alignment: .center, spacing: 16) {
+            Image(animal.image)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 90, height: 90)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text(animal.name)
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundStyle(.accent)
+                
+                Text(animal.headline)
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+                    .padding(.trailing, 8)
+            }// VStack
+        }// HStack
+    }// Body
+}// View
+
+// MARK: - Preview
+#Preview {
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    AnimalListItemView(animal: animals[3])
+}
