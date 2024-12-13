48e4016c0b9e05fcba2c39ca98b91042517cb858
Vlad <160039489+55kt@users.noreply.github.com>
2024-12-13 18:29:38 +0200

    Decode JSON file fucn & fetch data

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -8,11 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - Properties
+    
+    // MARK: - Body
     var body: some View {
-        Text("Content")
-    }
-}
+        NavigationView {
+            List {
+                CoverImageView()
+                    .frame(height: 300)
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            }// List
+            .navigationBarTitle("Africa", displayMode: .large)
+        }// NavigationView
+    }// Body
+}// View
 
+// MARK: - Preview
 #Preview {
     ContentView()
 }
---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,14 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Vlad on 13/12/24.
+//
+
+import Foundation
+
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
