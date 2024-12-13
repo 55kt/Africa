597b71872d31701a989e7d09c18aa998d40a5d66
Vlad <160039489+55kt@users.noreply.github.com>
2024-12-13 17:10:53 +0200

    First Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Vlad on 13/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
