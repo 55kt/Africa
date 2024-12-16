100bc7cc71de68e726e2fdbaa05576fdc069ea93
Vlad <160039489+55kt@users.noreply.github.com>
2024-12-16 17:18:39 +0200

    Video list & Video player

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct VideoListView: View {
+    // MARK: - Properties
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+    
+    // MARK: - Body
     var body: some View {
-        Text("Videos")
-    }
-}
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    VideoListItemView(video: item)
+                        .padding(.vertical, 8)
+                }// ForEach
+            }// List
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        videos.shuffle()
+                        hapticImpact.impactOccurred()
+                    }) {
+                        Image(systemName: "arrow.2.squarepath")
+                    }// Button
+                }// ToolbarItem
+            }// toolbar
+        }// NavigationView
+    }// Body
+}// View
 
+// MARK: - Preview
 #Preview {
     VideoListView()
 }
