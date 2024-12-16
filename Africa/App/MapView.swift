c5b1038c4bd600c3823a09eb12a3eb3c18799cc1
Vlad <160039489+55kt@users.noreply.github.com>
2024-12-16 20:01:07 +0200

    Custom map annotation

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,13 +6,46 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    // MARK: - Properties
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 60.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        
+        return mapRegion
+    }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
+    // MARK: - Body
     var body: some View {
-        Text("Map")
-    }
-}
+        // MARK: - No1 Basic map
+//        Map(coordinateRegion: $region)
+        
+        // MARK: - No2 Advanced map
+        Map(coordinateRegion: $region, annotationItems: locations, annotationContent: {
+            item in
+            // (A) PIN: Old style (always static)
+//            MapPin(coordinate: item.location, tint: .accentColor)
+            
+            // (B) MARKER: New style (always static)
+//            MapMarker(coordinate: item.location, tint: .accent)
+            
+            // (C) Custom basic annotation (it could be interactive)
+            MapAnnotation(coordinate: item.location) {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32, alignment: .center)
+            }// MapAnnotation
+        })// Map
+    }// Body
+}// View
 
+// MARK: - Preview
 #Preview {
     MapView()
 }
